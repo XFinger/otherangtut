f70606825a571b89bcf5c881b4aef492ddb83dbf
xfinger <xfinger@gmail.com>
2018-04-21 12:42:31 -0400

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,36 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterializeModule } from 'angular2-materialize';
+import { Angular2TokenService } from 'angular2-token';
+import { HomeComponent } from './home/home.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AuthDialogComponent } from './auth-dialog/auth-dialog.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import {AuthService} from "./services/auth.service";
+import { ProfileComponent } from './profile/profile.component';
+import {AuthGuard} from "./guards/auth.guard";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ToolbarComponent,
+    AuthDialogComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
+    ProfileComponent
+   // ProfileComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterializeModule
   ],
-  providers: [],
+  providers: [Angular2TokenService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
